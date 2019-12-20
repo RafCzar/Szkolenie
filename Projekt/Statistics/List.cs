b90f05ad3950b702e3fc3088f0b1b555495969e9
student <student@DESKTOP-MK87L37>
2019-12-20 10:44:40 +0100

    klasa List

---
+++ Projekt/Statistics/List.cs
@@ -0,0 +1,52 @@
+﻿using Projekt.Db;
+using Projekt.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Statistics
+{
+    class List
+    {
+        private IReader reader;
+
+        public List(IReader reader)
+        {
+            this.reader = reader;
+        }
+
+        private void DisplayLine(Item item)
+        {
+            string type = "";
+
+            switch (item.Type)
+            {
+                case ItemType.Income:
+                    type = "DOCHÓD";
+                    break;
+                case ItemType.Outcome:
+                    type = "KOSZT";
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"{item.Id} {item.Name} {type} {item.Amount} {item.Date}");
+        }
+
+        public void DisplayList()
+        {
+            IEnumerable<Item> list = reader.ReadAll();
+
+            foreach (Item item in list)
+            {
+                DisplayLine(item);
+            }
+        }
+
+
+
+    }
+}
