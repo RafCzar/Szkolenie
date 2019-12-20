e0aeb0b4f3c7f2539fbe1eae6b6a621054e09a19
student <student@DESKTOP-MK87L37>
2019-12-20 14:20:26 +0100

    EntityFramework

---
+++ lab38 - Entity/Program.cs
@@ -0,0 +1,30 @@
+﻿using lab38___Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab38___Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bikes = new List<Bike>
+            {
+                new Bike{ Number = "001", BikeType = BikeType.Mountain},
+                new Bike{ Number = "004", BikeType = BikeType.Road},
+                new Bike{ Number = "008", BikeType = BikeType.Town},
+                new Bike{ Number = "011", BikeType = BikeType.Mountain},
+                new Bike{ Number = "012", BikeType = BikeType.Road},
+            };
+
+            using (var context = new RentBikeContext())
+            {
+                context.Bikes.AddRange(bikes);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ lab38 - Entity/RentBikeContext.cs
@@ -0,0 +1,21 @@
+﻿using lab38___Entity.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab38___Entity
+{
+    class RentBikeContext : DbContext
+    {
+        public DbSet<Bike> Bikes {get; set;}
+        public DbSet<Rent> Rents {get; set;}
+
+        public RentBikeContext() : base("ttt") 
+        {
+        }
+
+    }
+}
